3bfbfd397dddfc9a454b5904fd4e9f27e0545238
Ian <2over124@gmail.com>
2021-05-28 16:08:20 -0400

    immediates envrionment

--- lib/ImmEnv.ml
+++ lib/ImmEnv.ml
@@ -6,4 +6,6 @@ Boolean variables are represented by an Envrionment s.t. bvar is true
 *)
 module Env = MapDomain.MakeMap(Var)(ClpDomain)
 
-let x = 1
+
+
+module Immediates = ReducedProduct.MakeProduct(Env)(MapDomain.MakeMap(Var)(Env))
