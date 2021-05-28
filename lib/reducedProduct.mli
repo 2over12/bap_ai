f9d06b86c4f5aeae87479161e7cfe397ed255df7
Ian <2over124@gmail.com>
2021-05-28 16:05:22 -0400

    reduced product

--- lib/reducedProduct.mli
+++ lib/reducedProduct.mli
@@ -1,1 +1,6 @@
-module MakeProduct(X: AbstractDomain.SET)(Y: AbstractDomain.CPO): CPO
+module type CPO = sig 
+    include AbstractDomain.CPO
+end
+ 
+module MakeProduct(X: AbstractDomain.CPO)(Y: AbstractDomain.CPO): CPO
+
